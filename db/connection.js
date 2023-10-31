cd79fc083b7d7592f91edc1dbfa39d76be5cdd59
Shivani Singh <shivanisingh36813@gmail.com>
2023-10-31 12:04:15 +0530

    add env

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/booksAPI', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
